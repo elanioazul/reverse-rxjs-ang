dd79fc5320005d507d0f8143dcfd72007e38e28f
Hugo Herrador <at_dessig21@mapa.es>
2022-06-03 14:49:46 +0200

    More function are subscribed and each value emmited is also passed through all func subscribed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,23 @@ export class AppComponent implements OnInit {
   printFunction(thing: string) {
     console.log(`I will print the ${thing}`);
   }
+  multiplyBy2(num: number) {
+    console.log(num * 2)
+  }
+  sum2(num: number) {
+    console.log(num + 2)
+  }
   ngOnInit(): void {
     const o = new Observable();
     o.subscribe(this.printFunction);
+    o.subscribe(this.multiplyBy2);
+    o.subscribe(this.sum2);
+
     o.emit("Apple");
     o.emit("Orange");
     o.emit("Pear");
+    
+    o.emit(4)
   }
 
 }
