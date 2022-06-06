7c506df0026a27b6d36d1c706cd969eb0b25b838
Hugo Herrador <at_dessig21@mapa.es>
2022-06-06 12:25:56 +0200

    Creates a tap method that collects our data in between pipe. For this it takes a function and a value and it applys function with the value and return the original value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,10 @@ export class AppComponent implements OnInit {
 
   private pipe = (f, g) => x => g(f(x));//pipe function to compose function, kind of currying function
   private improvedPipe = (...funcs) => x => funcs.reduce((effects, f) => f(effects), x);//to be able to pass in any numbers of functions and compose them
+  private tap = fun => x => {
+    fun(x);
+    return x;
+  };
 
   printFunction(thing: any) {
     console.log(`I will print the ${thing}`);
@@ -32,6 +36,7 @@ export class AppComponent implements OnInit {
       this.improvedPipe(
         this.square,
         this.multiplyBy2,
+        this.tap(this.printFunction),
         this.square,
         this.printFunction
       )
