ec695433c903afd6b2542098b1c405c0ca5f14ca
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-06-13 14:43:00 +0200

    Makes the map operator callback function too restrictive

--- src/app/components/pizarra/pizarra.component.ts
+++ src/app/components/pizarra/pizarra.component.ts
@@ -9,6 +9,7 @@ import {map} from 'rxjs/operators';
 export class PizarraComponent implements OnInit {
 
   public emiter: Observable<number> = of(1,2,3,4);
+  public anotherEmiter: Observable<number> = of(1,2);
   public emitArr: Observable<number> = from([5,6,7,8]);
   constructor() { }
 
@@ -18,31 +19,43 @@ export class PizarraComponent implements OnInit {
     //     console.log(`value is at this point of the stream: ${val}`)
     //   }
     // })
-    const remainder = this.emiter.pipe(
-      map((val: number) => {
-        console.log(`recieved value: ${val}`);
-        return val % 2;
-      })
-    )
-    const porDosYString = this.emiter.pipe(
-      map((val: number) => { return val * 2 }),
-      map((val: number) => { return `str_${val}` })
-    )
+    // const remainder = this.emiter.pipe(
+    //   map((val: number) => {
+    //     console.log(`recieved value: ${val}`);
+    //     return val % 2;
+    //   })
+    // )
+    // const porDosYString = this.emiter.pipe(
+    //   map((val: number) => { return val * 2 }),
+    //   map((val: number) => { return `str_${val}` })
+    // )
     // remainder.subscribe((val:any) => {
     //   console.log(`remainder: ${val}`)
     // })
-    porDosYString.subscribe((val:string) => {
-      console.log(`stringMap emmited: ${val}`)
-    })
+    // porDosYString.subscribe((val:string) => {
+    //   console.log(`stringMap emmited: ${val}`)
+    // })
 
-    const swallowingValues = this.emiter.pipe(
+    // const swallowingValues = this.emiter.pipe(
+    //   map((val: number) => {
+    //     console.log(`swallowing the value ${val}`)
+    //     //forgot to return a value
+    //   })
+    // )
+    // swallowingValues.subscribe((val: void) => {
+    //   console.log(`subscriber recieved value : ${val}`)
+    // })
+
+    const swallogingAgain = this.anotherEmiter.pipe(
       map((val: number) => {
-        console.log(`swallowing the value ${val}`)
-        //forgot to return a value
+        if (val < 2) {
+          return null;
+        }
+        return val
       })
-    )
-    swallowingValues.subscribe((val: void) => {
-      console.log(`subscriber recieved value : ${val}`)
+    );
+    swallogingAgain.subscribe((val: number | null) => {
+      console.log(`subscriber recieved value: ${val}`)
     })
   }
 
